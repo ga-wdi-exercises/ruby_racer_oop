c7b319cd4b156c0fe2e06dcd3da63454c559d994
Rob Romaniw <robertromaniw@gmail.com>
2016-10-27 13:47:50 -0400

    Racer Bonuses

--- lib/racer.rb
+++ lib/racer.rb
@@ -23,5 +23,15 @@ class Racer
     #min_by will let u compare a specific thing inside of an array...u cant use just .min
   end
 
+  def best_avg_speed type, distance
+    best = best_race type, distance
+    best.avg_speed
+  end
+
+  def fastest_km_time type
+    sel_races = @races.select{|race| race.race_type == type}
+    all_times = sel_races.map {|race| race.km_times}
+    all_times.flatten.min
+    end
 
 end
