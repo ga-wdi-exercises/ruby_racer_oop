c68a0ede1f17f38da40ce81b918eba9f8f846cdf
robert <robert.k25@gmail.com>
2016-10-27 11:38:15 -0400

    gets the remaining tests passing

--- lib/race.rb
+++ lib/race.rb
@@ -11,4 +11,16 @@ attr_reader :name,  :race_type, :distance, :km_times
     @km_times.map(&:to_i).reduce(0, :+)
     #makes sure values are ints and adds them up
   end
+
+  def quickest_km
+    @km_times.min
+  end
+
+  def slowest_km
+    @km_times.max
+  end
+
+  def avg_time_per_km
+    @km_times.reduce(:+).to_f / @km_times.size
+  end
 end
