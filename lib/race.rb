7fd235480db6495964dd2562ddd36607c9b619d1
Thinh Nguyen <nguyendthinh@outlook.com>
2016-10-27 12:19:33 -0400

    finished errors in rspec

--- lib/race.rb
+++ lib/race.rb
@@ -1,3 +1,28 @@
 class Race
 
+  attr_accessor :name, :race_type, :distance, :km_times
+
+  def initialize name, race_type, distance, km_times
+    @name = name
+    @race_type = race_type
+    @distance = distance
+    @km_times = km_times
+  end
+
+  def total_time
+    km_times.inject(0, :+)
+  end
+
+  def slowest_km
+    km_times.max
+  end
+
+  def quickest_km
+    km_times.min
+  end
+
+  def avg_time_per_km
+    (self.total_time / @km_times.length.to_f).round(2)
+  end
+
 end
--- lib/racer.rb
+++ lib/racer.rb
@@ -1,3 +1,21 @@
-class Racer
+require_relative "race"
 
-end
+class Racer < Race
+
+  attr_accessor :name
+  attr_reader :races
+
+  def initialize name
+    @name = name
+    @races = []
+  end
+
+  def add_race race
+    @races << race
+  end
+
+  def best_race race_type, distance
+      selections = @races.select {|race| race.race_type == race_type && race.distance == distance}
+      selections.min_by {|race| race.total_time}
+    end
+  end
