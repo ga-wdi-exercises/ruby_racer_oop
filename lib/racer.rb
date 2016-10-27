21348cc1bab248a9b4781c3b02ac3e421473cced
Drew Swinburne <drewswinburne@gmail.com>
2016-10-27 13:07:24 -0400

    initial w two bonuses

--- lib/racer.rb
+++ lib/racer.rb
@@ -1,3 +1,25 @@
+
 class Racer
+  attr_accessor :name
+  attr_reader :races
+
+  def initialize name, races = []
+    @name = name
+    @races = races
+  end
+
+  def add_race race
+    races << race
+  end
+
+  def best_race type, length
+    goodraces = @races.select {|n| n.race_type == type && n.distance == length}
+    bestraces = goodraces.sort_by do |y|
+      y.total_time
+    end
+    bestraces[0]
+
+end
+
 
 end
