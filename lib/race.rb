741f41c7a42c93874fcfb3c7c387db433b36e96e
Sam Catherman <Sam.Catherman@gmail.com>
2016-10-27 12:20:25 -0400

    passes all but 3 tests

--- lib/race.rb
+++ lib/race.rb
@@ -1,3 +1,21 @@
 class Race
-
+  attr_accessor :name, :race_type, :distance, :km_times, :total_time
+  def initialize name, race_type, distance, km_times
+    @name = name
+    @race_type = race_type
+    @distance = distance
+    @km_times = km_times
+  end
+  def total_time
+    return km_times.inject(:+)
+  end
+  def slowest_km
+    return km_times.max
+  end
+  def quickest_km
+    return km_times.min
+  end
+  def avg_time_per_km
+    return total_time/km_times.size
+  end
 end
--- lib/racer.rb
+++ lib/racer.rb
@@ -1,3 +1,11 @@
 class Racer
+  attr_accessor :name, :races
+  def initialize name
+      @name = name
+      @races = []
+  end
 
+  def add_race(race)
+    @races << race
+  end
 end
