1f809cfd7baeb74d370c3a6da02da1bb152e4823
Thinh Nguyen <nguyendthinh@outlook.com>
2016-10-27 18:16:35 -0400

    did bonus racers spec

--- lib/racer.rb
+++ lib/racer.rb
@@ -17,5 +17,17 @@ class Racer < Race
   def best_race race_type, distance
       selections = @races.select {|race| race.race_type == race_type && race.distance == distance}
       selections.min_by {|race| race.total_time}
-    end
+  end
+
+  def best_avg_speed race_type, distance
+    bestrace = best_race race_type, distance
+    bestrace.avg_speed
+  end
+
+  def fastest_km_time race_type
+    selections = @races.select {|race| race.race_type == race_type}
+    bestrace = selections.min_by {|race| race.total_time}
+    bestrace.quickest_km
+  end
+
   end
