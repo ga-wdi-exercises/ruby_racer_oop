b0117230b82c8702690b165497334b3efd8eb2b2
robert <robert.k25@gmail.com>
2016-10-27 11:02:12 -0400

    adds race properties

--- lib/race.rb
+++ lib/race.rb
@@ -1,3 +1,11 @@
 class Race
+attr_reader :name,  :race_type, :distance, :km_times
+  def initialize(name, race_type, distance, km_times)
+    @name = name
+    @race_type = race_type
+    @distance = distance
+    @km_times = km_times
+  end
+
 
 end
