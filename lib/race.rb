9d876ded7316de0afb976e7ee1a95ae53a19aafc
Jonathan Grunewald <jonathan.grunewald1@gmail.com>
2016-10-27 10:35:05 -0400

    completes all but bonus

--- lib/race.rb
+++ lib/race.rb
@@ -1,3 +1,28 @@
 class Race
+  attr_accessor :name, :race_type, :distance, :km_times
 
+  def initialize name, type, distance, km_times =[]
+    @name = name
+    @race_type = type
+    @distance = distance
+    @km_times = km_times
+  end
+
+  def total_time
+    @km_times.reduce(:+)
+  end
+
+  def slowest_km
+    @km_times.max
+  end
+
+  def quickest_km
+    @km_times.min
+  end
+
+  def avg_time_per_km
+    @km_times.reduce(:+).to_f.round(2)/@km_times.length
+  end
+
+  
 end
--- lib/racer.rb
+++ lib/racer.rb
@@ -1,3 +1,21 @@
 class Racer
+  attr_accessor :name
+  attr_reader :races
+
+  def initialize name
+    @name = name
+    @races = []
+  end
+
+  def add_race race
+    @races << race
+  end
+
+  def best_race type, distance
+    sel_races = @races.select{|race| race.race_type == type && race.distance == distance}
+    total_time = sel_races.map {|race| race.total_time}
+    race_index = total_time.index(total_time.min)
+    return sel_races[race_index]
+  end
 
 end
